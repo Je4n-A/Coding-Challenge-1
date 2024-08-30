b3eba83438cb8edd299ac7d062ea9af1fe5bca8a
Je4n-A <jeanma6742@gmail.com>
2024-08-30 01:30:36 -0400

    "Task 2 - Product Inventory Management".

--- cc_1.js
+++ cc_1.js
@@ -9,3 +9,15 @@ console.log(employeeID, typeoff(employeeID));
 console.log(isActive, typeoff(isActive));
 
 
+
+//Task 2: Compound Data Types 
+
+let products = ["Iphone", "Ipad", "Macbook"];
+const productDetails = {
+    name: products, 
+    price: 1000.00,
+    inStock: 50, 
+}
+
+console.log(products, typeoff(products))
+console.log(productDetails, typeoff(productDetails))

5f1a5c6b331dedc43043fc694c261cee11a721a0
Je4n-A <jeanma6742@gmail.com>
2024-08-30 00:53:22 -0400

    Task 1: Variables and Data Types

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "John Snow";
+const employeeID = 213;
+var isActive = true;
+
+console.log(employeeName, typeoff(employeeName));
+console.log(employeeID, typeoff(employeeID));
+console.log(isActive, typeoff(isActive));
+
+
